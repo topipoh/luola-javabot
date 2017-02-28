4218bc157affe7cc86ee805d6ad1e9ec3a554c23
Topi Pohjosaho <topi.pohjosaho@solita.fi>
2017-02-28 12:08:40 +0200

    added possibility to run multiple bots

--- src/main/java/javabot/Bot.java
+++ src/main/java/javabot/Bot.java
@@ -1,22 +1,78 @@
 package javabot;
 
+import static java.util.stream.Collectors.toList;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.apache.commons.lang3.tuple.Pair;
+
 import javabot.api.API;
+import javabot.api.Action;
+import javabot.api.Direction;
 
 public class Bot {
-
-	public static void main(String[] args) throws Exception {
-		Bot bot = new Bot();
-		bot.run();
+	
+	public static final String NAME = "SMULTR0N";
+	public static final String PASS = "SMULTR0N DESTROY!!1";
+	private final String name;
+	private final String pass;
+	
+	public Bot(String name, String pass) {
+		this.name = name;
+		this.pass = pass;
 	}
-
-	public void run() throws Exception {
-		System.out.println("Running bot!");
-		API.addPlayer();
-		while(true) {
-			final String board = API.getBoard();
-			API.act(new AI(board).takeAction());
-			System.out.println(board);
-			Thread.sleep(100);
+	
+	public static void main(String[] args) throws Exception {
+		for(Pair<String, String> pair : getBots(args)) {
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					Bot bot = new Bot(pair.getLeft(), pair.getRight());
+					bot.run();
+				}
+			}).start();
 		}
 	}
+	
+	public void run() {
+		System.out.println("Running bot " + name);
+		API.addPlayer(name, pass);
+		while(true) {
+			final String board = API.getBoard(name);
+			Pair<Action, Direction> action = new AI(board).takeAction();
+			System.out.println(name + ": " + action);
+			API.act(name, pass, action);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static List<Pair<String, String>> getBots(String[] args) {
+		Integer bots = parseBotsCount(args);
+		if(bots == null) {
+			return Arrays.asList(Pair.of(NAME, PASS));
+		} else {
+			return IntStream.rangeClosed(1, bots)
+					.mapToObj(i -> (Integer) i)
+					.map(i -> Pair.of("javabot-" + i, "javabot-" + i))
+					.collect(toList());
+		}
+	}
+
+	private static Integer parseBotsCount(String[] args) {
+		if(args.length == 1) {
+			try {
+				return Integer.parseInt(args[0]);
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
 }
--- src/main/java/javabot/api/API.java
+++ src/main/java/javabot/api/API.java
@@ -10,35 +10,31 @@ public class API {
 	public static final String BOARD = BASE_URL + "/board";
 	public static final String ACT = BASE_URL + "/act";
 	public static final String ADD_PLAYER = BASE_URL + "/add-player";
-	public static final String NAME = "SMULTR0N";
-	public static final String PASS = "SMULTR0N DESTROY!!1";
 	
-	public static void addPlayer() {
+	public static void addPlayer(String name, String pass) {
 		with()
-			.param("name", NAME)
-			.param("pass", PASS)
+			.param("name", name)
+			.param("pass", pass)
 			.get(ADD_PLAYER);
 	}
 
-	public static String getBoard() {
+	public static String getBoard(String name) {
 		return with()
-				.param("name", NAME)
+				.param("name", name)
 				.get(BOARD)
 				.asString();
 	}
 	
-	public static void act(Pair<Action, Direction> pair) {
-		act(pair.getLeft(), pair.getRight());
+	public static void act(String name, String pass, Pair<Action, Direction> pair) {
+		act(name, pass, pair.getLeft(), pair.getRight());
 	}
 
-	public static void act(Action action, Direction direction) {
+	public static void act(String name, String pass, Action action, Direction direction) {
 		with()
-			.param("name", NAME)
-			.param("pass", PASS)
+			.param("name", name)
+			.param("pass", pass)
 			.param("action", action)
 			.param("target", direction)
 		.get(ACT);
 	}
-
-
 }
